def9cc4940ef44a499857b80771def2d13657ebe
abdul8840 <abdul14941naseer@gmail.com>
2024-07-15 19:03:32 +0530

    create and add fully functional product create page

--- components/collections/CollectionForm.tsx
+++ components/collections/CollectionForm.tsx
@@ -81,7 +81,7 @@ const CollectionForm: React.FC<CollectionFormProps> = ({ initialData }) => {
     <div className="p-10">
       {initialData ? (
         <div className="flex items-center justify-between">
-          <p className="text-heading2-bold">Update Collection</p>
+          <p className="text-heading2-bold">Edit Collection</p>
           <Delete id={initialData._id} item={''} />
         </div>
       ) : (
